553f9b9671427403e7a188e78f6c28ee93acc6a7
Ilya Loginov <58655234+despair101@users.noreply.github.com>
2022-04-06 18:46:39 +0700

    Update bruteforce.cpp

--- bruteforce.cpp
+++ bruteforce.cpp
@@ -3,31 +3,28 @@ vector<int> answer;
 vector<int> used;
 
 void recursive(int N, int W, const vector<int>& w, int curW, int index) {
-  if (index == N) {
-    if (bestW < curW && curW <= W) {
-      bestW = curW;
-      answer = used;
+    if (index == N) {
+        if (bestW < curW && curW <= W) {
+            bestW = curW;
+            answer = used;
+        }
+    } else {
+        recursive(N, W, w, curW, index + 1);
+        used.push_back(index);
+        recursive(N, W, w, curW + w[index], index + 1);
+        used.pop_back();
     }
-  } else {
-    recursive(N, W, w, curW, index + 1);
-    used.push_back(index);
-    recursive(N, W, w, curW + w[index], index + 1);
-    used.pop_back();
-  }
 }
 
 void solve(int N, int W, const vector<int>& w) {
-  bestW = 0;
-  answer.clear();
-  used.clear();
-  recursive(N, W, w, 0, 0);
-  cout << "N = " << N << ", W = " << W << "\nw = [ ";
-  for (int el : w) {
-    cout << el << ' ';
-  }
-  cout << "]\nBest sum = " << bestW << "\nIndexes for this sum (from 1 to N):\n";
-  for (int el : answer) {
-    cout << el + 1 << ' ';
-  }
-  cout << '\n';
+    recursive(N, W, w, 0, 0);
+    cout << "N = " << N << ", W = " << W << "\nw = [ ";
+    for (int el : w) {
+        cout << el << ' ';
+    }
+    cout << "]\nBest sum = " << bestW;
+    cout << "\nIndexes for this sum (from 1 to N):\n";
+    for (int el : answer) {
+        cout << el + 1 << ' ';
+    }
 }
